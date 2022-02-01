64d5a652152740a704bbe48ff5c0f522ab85c50f
Sumant9822 <sumantsantosh@benchmarkit.solutions>
2022-02-01 10:08:33 +0530

    .

--- CRM_Project/src/app/person.model.ts
+++ CRM_Project/src/app/person.model.ts
@@ -1,4 +1,4 @@
-export class Person{
-  constructor(public person_id: any , public first_name: string, public last_name:string, public gender: any,
-     public DOB: Date, public zipcode: number, public address_id: any){}
+export class Plan {
+    constructor(public first_name: any, public last_name: any,
+    ) { }
 }
