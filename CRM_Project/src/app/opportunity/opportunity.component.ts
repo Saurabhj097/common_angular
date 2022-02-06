fab57fcc38861447779d7828e843e2b4ff70ec29
Jui-ka <trainee23@benchmarkit.digital>
2022-02-06 15:32:12 +0530

    opportunity

--- CRM_Project/src/app/opportunity/opportunity.component.ts
+++ CRM_Project/src/app/opportunity/opportunity.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { OpportunityService } from './opportunity.service';
 
 @Component({
   selector: 'app-opportunity',
@@ -6,10 +8,124 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./opportunity.component.css']
 })
 export class OpportunityComponent implements OnInit {
+  errors: any;
 
-  constructor() { }
+
+  constructor(private os: OpportunityService, private fb: FormBuilder) { }
+
+  opps: any;
+  // selectForm: FormGroup;
+  cname: any;
+  cvalue: any;
+
+  choices: any = ['Id', 'Name', 'Email'];
 
   ngOnInit(): void {
+    this.getallOpps();
+
+  }
+
+  selectForm = this.fb.group({
+    choiceName: ['', [Validators.required]],
+    choiceValue: ['', [Validators.required]],
+  })
+
+  getChoice() {
+
+    this.cname = this.selectForm.value['choiceName'];
+    this.cvalue = this.selectForm.value['choiceValue'];
+    //console.log(this.cname);
+    console.log(this.cvalue);
+    if (this.cname == "Email") {
+      this.os.onLeadSearch('email', this.cvalue).subscribe(
+        result => {
+          console.log(result);
+          this.opps = result;
+        },
+        error => {
+          this.errors = error
+        });
+
+    }
+    else if (this.cname == "name") {
+
+      this.os.onLeadSearch('first_name', this.cvalue).subscribe(
+        result => {
+          console.log(result);
+          this.opps = result;
+        },
+        error => {
+          this.errors = error
+        });
+
+    }
+
+    else {
+
+      this.os.onLeadSearch('lead_id', this.cvalue).subscribe(
+        result => {
+          console.log(result);
+          this.opps = result;
+        },
+        error => {
+          this.errors = error
+        });
+
+    }
+  }
+
+  convertOpp(id: any, pId: any) {
+    console.log("working");
+    this.os.convertOpp(id, pId)
+      .subscribe(
+        response => {
+          console.log("hi");
+          // this.displayLead="none";
+          // this.getallLeads();
+          // <HTMLInputElement><unknown>document.getElementById("convertLead")?.className=btn btn-disabled;
+        },
+      )
+  }
+
+  getallOpps() {
+    this.os.getOpps().subscribe(result => {
+      this.opps = result;
+      console.log(result);
+    }
+    );
+    console.log(this.opps);
+  }
+
+  onIdSort() {
+    this.os.onIdSort().subscribe(
+      result => {
+        this.opps = result;
+      }
+    )
+  }
+
+  onNameSort() {
+    this.os.onNameSort().subscribe(
+      result => {
+        this.opps = result;
+      }
+    )
+  }
+
+  onDateSort() {
+    this.os.onDateSort().subscribe(
+      result => {
+        this.opps = result;
+      }
+    )
+  }
+
+  openModalEdit(i: any,) {
+
+  }
+
+  onCloseHandled() {
+
   }
 
 }
--- CRM_Project/src/app/opportunity/opportunity.service.ts
+++ CRM_Project/src/app/opportunity/opportunity.service.ts
@@ -10,14 +10,55 @@ import { map } from 'rxjs/operators';
 export class OpportunityService {
 
   private oppurl = 'http://localhost/benchmark_yii_projects/crm/crm_angular/frontend/web/index.php/opportunity';
-
+  private sortDateUrl = 'http://localhost/yii/crmNg/frontend/web/index.php/opportunities?sort=updated_at';
+  private sortfNameUrl = 'http://localhost/yii/crmNg/frontend/web/index.php/opportunities?sort=first_name';
+  private sortIdUrl = 'http://localhost/yii/crmNg/frontend/web/index.php/opportunities?sort=lead_id';
+  private oppSearchUrl = 'http://localhost/yii/crmNg/frontend/web/index.php/opportuniites?filter'
   constructor(private httpClient: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true',
+    }),
+  };
 
   getOpportunity(): Observable<any> {
     // this.httpClient.get(this.planurl).pipe(map(res => console.log(res)));
     // console.log('test');
 
     return this.httpClient.get(this.oppurl)
+  }
+
+  getOpps(): Observable<any> {
+    // console.log(`${this.leadUrl}/${leadid}`);
+    return this.httpClient.get(`${this.oppurl}?per-page=2&page=1`);
+  }
+
+  onLeadSearch(field: any, value: any) {
+    return this.httpClient.get(`${this.oppSearchUrl}[${field}][like]=${value}`);
+  }
+
+  convertOpp(id: any, pId: any) {
+    let data = {
+      "lead_id": id,
+      "person_id": pId
+    }
+    return this.httpClient.post(`${this.oppurl}/convert`, data);
+    console.log(data);
+  }
+
+  onIdSort() {
+    return this.httpClient.get(`${this.sortIdUrl}`);
+  }
+
+  onNameSort() {
+    return this.httpClient.get(`${this.sortfNameUrl}`);
+  }
+
+  onDateSort() {
+    return this.httpClient.get(`${this.sortDateUrl}`)
   }
 }
 
